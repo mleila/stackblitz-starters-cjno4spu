fc3efd84a1ec91ea355cf07dd7e15d03b5ec49f0
mleila <mohamed.leila.1989@gmail.com>
2025-08-06 14:20:55 -0400

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary': {
+          DEFAULT: 'rgb(39,54,105)',
+          foreground: 'rgb(240,241,246)', // foreground color for text
+        },
+        'secondary': {
+          DEFAULT: 'rgb(240,241,246)',
+          foreground: 'rgb(0, 0, 0)' // foreground color for text
+        },
+        'accent': {
+          DEFAULT: 'rgb(250,167,28)',
+          foreground: 'rgb(0, 0, 0)', // foreground color for text
+        },
       },
     },
   },
